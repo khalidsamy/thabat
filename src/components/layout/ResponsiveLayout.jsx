6c7d4bafc9869ed5588a9c097206dedaa591478a
Khalid Samy Abd-Elzaher <khalidsamy1231@gmail.com>
2026-04-04 18:41:25 +0200

    feat: add ResponsiveLayout component with sidebar, navbar, and bottom navigation integration

--- src/components/layout/ResponsiveLayout.jsx
+++ src/components/layout/ResponsiveLayout.jsx
@@ -1,4 +1,8 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../Sidebar';
+import BottomNav from '../BottomNav';
+import Navbar from '../Navbar';
 
 const ResponsiveLayout = () => {
   return (
