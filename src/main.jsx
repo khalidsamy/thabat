72be9d0cf2ffd26a899757c3e17e2320e16b9ca6
Khalid Samy Abd-Elzaher <khalidsamy1231@gmail.com>
2026-04-04 14:56:33 +0200

    feat: integrate Vercel Analytics into main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
